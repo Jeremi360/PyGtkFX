98741a172835fd75d77537b3a19851c728ecca15
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-26 10:58:21 +0200

    / ‘grabbo/__init__.py’

--- grabbo/__init__.py
+++ grabbo/__init__.py
@@ -1,11 +1,12 @@
 import os as _os
 import importlib as _imp
-imports = _os.listdir(_os.path.join('.'))
+imports = _os.listdir('.')
 print(imports)
 for i in imports:
-    n = i.split('.')[0]
-    p = "grabbo." + n
-    _imp.import_module(n.capitalize(), p)
+    if i.split('.')[1] == 'py':
+        n = i.split('.')[0]
+        p = "grabbo." + n
+        _imp.import_module(n.capitalize(), p)
 
 
 
