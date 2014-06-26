ebdb318e151045ea7d1f788f84f4bad0a3f4bb92
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-26 09:41:27 +0200

    + ‘grabbo/__init__.py’ + ‘ui/Tab.ui’ + ‘ui/TabButton.ui’

---
+++ grabbo/__init__.py
@@ -0,0 +1,22 @@
+from gi.repository import Gtk
+import os
+
+class Builder(object):
+    def __init__(self, UI_FILE):
+        #load UI from UI_FILE
+        self.ui = Gtk.Builder()
+        self.ui.add_from_file(UI_FILE)
+        self.ui.connect_signals(self)
+
+class Window(Builder, Gtk.Window):
+    def __init__(self, content = Gtk.Box()):
+        Gtk.Window.__init__(self)
+        self.content = content
+        self.do_then_init()
+
+        self.set_size_request(400, 400)
+        self.add(self.content)
+        self.maximize()
+
+    def do_then_init(self):
+        pass
