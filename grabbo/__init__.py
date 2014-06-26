b15190008f4654c8b35a88e6c0e63dd2f363c4b1
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-26 10:59:20 +0200

    / ‘grabbo/__init__.py’

--- grabbo/__init__.py
+++ grabbo/__init__.py
@@ -3,10 +3,9 @@ import importlib as _imp
 imports = _os.listdir('.')
 print(imports)
 for i in imports:
-    if i.split('.')[1] == 'py':
-        n = i.split('.')[0]
-        p = "grabbo." + n
-        _imp.import_module(n.capitalize(), p)
+    n = i.split('.')[0]
+    p = "grabbo." + n
+     _imp.import_module(n.capitalize(), p)
 
 
 
