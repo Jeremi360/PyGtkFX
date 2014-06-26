a2f7c0172ef1eaa847015168cf696588d0521982
Jeremi Biernacki <jeremi360@gmail.com>
2014-06-26 11:04:54 +0200

    / ‘grabbo/__init__.py’

--- grabbo/__init__.py
+++ grabbo/__init__.py
@@ -1,11 +1,6 @@
-import os as _os
-import importlib as _imp
-imports = _os.listdir('.')
-print(imports)
-for i in imports:
-    n = i.split('.')[0]
-    p = "grabbo." + n
-    _imp.import_module(n.capitalize(), p)
+
+from grabbo.builder import Builder
+from grabbo.window import Window
 
 
 
