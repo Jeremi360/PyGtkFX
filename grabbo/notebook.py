e8d1a1abff9b2e41b16628a2727b9d321fcd5d19
Jeremi Biernacki <jeremi360@gmail.com>
2014-07-11 17:10:23 +0200

    / ‘grabbo/__pycache__/notebook.cpython-34.pyc’ / ‘grabbo/notebook.py’

--- grabbo/notebook.py
+++ grabbo/notebook.py
@@ -84,7 +84,7 @@ class Window(grabbo.Window):
         super(Window, self).__init__()
         Box = Gtk.VBox()
         N = Notebook()
-        Box.add(N.get(), N.tabs)
+        Box.add(N.get(), N.pages)
         self.add(Box)
         self.show()
 
