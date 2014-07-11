4f8cf4e440cc73a33d6c68f22eed9447d39d2b0e
Jeremi Biernacki <jeremi360@gmail.com>
2014-07-11 22:45:37 +0200

    / ‘grabbo/__pycache__/notebook.cpython-34.pyc’ / ‘grabbo/notebook.py’

--- grabbo/notebook.py
+++ grabbo/notebook.py
@@ -32,7 +32,11 @@ class _TabButton(grabbo.Builder):
         if self.button.get_active():
             self.notebook.pages.set_current_page(self.num)
         else:
-            self.notebook.pages.prev_page()
+            if self.notebook.pages.get_current_page() == self.num:
+                self.button.set_active(True)
+            else:
+                pass
+            #self.notebook.pages.prev_page()
 
     def on_close(self):
         self.notebook.pages.remove_page(self.num)
