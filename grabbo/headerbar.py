4221f1c89cf4a6b07366f2a6c3b07e844b9e64e7
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-14 09:21:20 +0200

    / ‘grabbo/headerbar.py’

--- grabbo/headerbar.py
+++ grabbo/headerbar.py
@@ -24,7 +24,7 @@ class test(Window):
         vp.add(self.box)
         scroll.add(vp)
 
-        button.connect("clicked", self.on_button)
+        button.connect("clicked", self.on_button("add"))
 
         box_zwie.pack_start(scroll, True, True, True)
         box_zwie.pack_end(button, True, True, True)
