10aac383fec5757587e99daf3048f5d1756cf788
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-14 10:10:48 +0200

    / ‘grabbo/notebook.py’

--- grabbo/notebook.py
+++ grabbo/notebook.py
@@ -16,9 +16,12 @@ class TabButton(grabbo.Builder):
 
     def set(self, notebook, num, label, closeable = True):
         self.set_closeable(closeable)
-        label = str(label)
-        self.button.set_label(label)
-        print(label)
+
+        try:
+            self.button.set_label(label)
+        except:
+            self.button.set_label(label.get_label())
+
         self.notebook = notebook
         self.num = num
 
