7e96455daaaff4a5be07cd2b622421c1c4ef7174
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-14 08:55:09 +0200

    / ‘grabbo/headerbar.py’

---
+++ grabbo/headerbar.py
@@ -0,0 +1,10 @@
+from gi.repository import Gtk
+
+class HeaderBar(Gtk.HeaderBar, Gtk.Box):
+    def __init__(self):
+        super(HeaderBar, self).__init__()
+        self.set_show_close_button(True)
+        self.set_title("")
+        self.props.border_width = 0
+        self.props.margin = 0
+        self.set_has_subtitle(False)
