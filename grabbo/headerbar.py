0058352815296a97fa0dcca8fa1a7091b22358a7
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-14 09:03:44 +0200

    / ‘grabbo/headerbar.py’

--- grabbo/headerbar.py
+++ grabbo/headerbar.py
@@ -3,12 +3,13 @@ from grabbo import Window
 
 class HeaderBar(Gtk.HeaderBar, Gtk.Box):
     def __init__(self):
-        super(HeaderBar, self).__init__()
+        Gtk.HeaderBar.__init__(self)
         self.set_show_close_button(True)
         self.set_title("")
         self.props.border_width = 0
         self.props.margin = 0
         self.set_has_subtitle(False)
+        Gtk.Box.__init__()
 
     def pack_start(self, child, expand, fill, padding):
         Gtk.Box.pack_start(self, child, expand, fill, padding)
