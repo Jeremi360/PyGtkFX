252ef80548c67d443bc78954e4d6740c93cafe22
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-14 08:23:37 +0200

    - ‘LICENSE’ - ‘README.md’ - ‘SparkleShare.txt’ - ‘grabbo/__init__.py’ - ‘grabbo/__pycache__/__init__.cpython-34.pyc’ - ‘grabbo/__pycache__/builder.cpython-34.pyc’ - ‘grabbo/__pycache__/helpers.cpython-34.pyc’ - ‘grabbo/__pycache__/notebook.cpython-34.pyc’ - ‘grabbo/__pycache__/window.cpython-34.pyc’ - ‘grabbo/builder.py’ ...

--- grabbo/window.py
+++
@@ -1,9 +0,0 @@
-from gi.repository import Gtk
-from grabbo import Builder
-
-class Window(Builder, Gtk.Window):
-    def __init__(self, content = Gtk.Box()):
-        Gtk.Window.__init__(self)
-        self.set_size_request(400, 400)
-        self.connect("destroy", Gtk.main_quit)
-        self.maximize()
