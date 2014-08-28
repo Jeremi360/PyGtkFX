a645bcf9328755b648122a98996076c54477cb70
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-28 16:34:23 +0200

    / ‘grabbo/notebook.py’

--- grabbo/notebook.py
+++ grabbo/notebook.py
@@ -36,9 +36,8 @@ class TabButton(grabbo.Builder):
         self.notebook.pages.remove_page(self.num)
         self.notebook.buttons_box.remove(self.get())
 
-Nbui = grabbo.ui.Notebook_ui #os.path.join("..", "ui", "notebook.ui")
 
-class Notebook():
+class Notebook(grabbo.ui.Notebook_ui): #os.path.join("..", "ui", "notebook.ui")
     def __init__(self, addable = True, closeable = True, orientation = Gtk.Orientation.HORIZONTAL):
         super(Notebook, self).__init__()
 
