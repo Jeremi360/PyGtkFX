819030d3cd1c26f600226a54d233342daa31b1f4
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-28 17:12:02 +0200

    / ‘grabbo/notebook.py’

--- grabbo/notebook.py
+++ grabbo/notebook.py
@@ -3,6 +3,9 @@ from gi.repository import Gtk
 import grabbo
 
 TBui = grabbo.ui.TAB_UI  #os.path.join('..', 'ui', 'TabButton.ui')
+gtknever = Gtk.PolicyType.NEVER
+gtknone = Gtk.ShadowType.NONE
+gtkright = Gtk.DirectionType.RIGHT
 
 class TabButton(grabbo.Builder):
     def __init__(self):
@@ -37,9 +40,53 @@ class TabButton(grabbo.Builder):
         self.notebook.buttons_box.remove(self.get())
 
 
-class Notebook(grabbo.ui.NOTEBOOK_UI): #os.path.join("..", "ui", "notebook.ui")
+class Notebook(Gtk.Box): #os.path.join("..", "ui", "notebook.ui")
     def __init__(self, addable = True, closeable = True, orientation = Gtk.Orientation.HORIZONTAL):
         super(Notebook, self).__init__()
+        self.props.hexpand = True
+        self.props.vexpand = False
+
+        self._scrolledwindow = Gtk.ScrolledWindow()
+        self._scrolledwindow.props.hexpand = True
+        self._scrolledwindow.props.vexpand = False
+        self._scrolledwindow.props.vscrollbar_policy = gtknever
+
+        self._viewport = Gtk.Viewport()
+        self._viewport.props.visible = True
+        self._viewport.props.can_focus = False
+        self._viewport.props.hexpand = True
+        self._viewport.props.vexpand = False
+        self._viewport.props.shadow_type = gtknone
+
+        self.ButtonBox = Gtk.Box()
+        self.ButtonBox.props.visible = True
+        self.ButtonBox.props.can_focus = False
+        self.ButtonBox.props.hexpand = True
+        self.ButtonBox.props.vexpand = False
+
+        #<property name="expand">True</property>
+        #<property name="fill">True</property>
+
+        AddIcon = Gtk.Image()
+        AddIcon.new_from_icon_name("list-add", 4)
+
+        self.Add = Gtk.Button()
+        self.Add.props.visible = True
+        self.Add.props.can_focus = True
+        self.Add.props.receives_default = True
+        self.Add.props.image = AddIcon
+        self.Add.props.relief = gtknone
+        self.Add.props.image_position = gtkright
+
+        #<property name="expand">False</property>
+        #<property name="fill">False</property>
+        #<property name="position">1</property>
+
+        self.pack_end(self.Add, True, True, True)
+        self._scrolledwindow.add(self._viewport)
+        self._viewport.add()
+        self.pack_start(self._scrolledwindow, True, True, True)
+        self.show_all()
 
         self.stack = Gtk.Stack()
         self.set_addable(addable)
