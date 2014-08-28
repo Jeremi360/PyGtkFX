c91a12caada8d3121c3242f5846cabdaee6b0f88
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-28 16:53:22 +0200

    / ‘grabbo/ui/notebook.py’

--- grabbo/ui/notebook.py
+++ grabbo/ui/notebook.py
@@ -55,56 +55,7 @@ class NOTEBOOK_UI(Gtk.Box):
         self._viewport.add(children)
 
 
-TAB_UI = '''<?xml version="1.0" encoding="UTF-8"?>
-<!-- Generated with glade 3.18.3 -->
-<interface>
-  <requires lib="gtk+" version="3.10"/>
-  <object class="GtkImage" id="CloseIcon">
-    <property name="visible">True</property>
-    <property name="can_focus">False</property>
-    <property name="icon_name">window-close</property>
-  </object>
-  <object class="GtkImage" id="TabIcon">
-    <property name="visible">True</property>
-    <property name="can_focus">False</property>
-    <property name="icon_name">applications-internet</property>
-  </object>
-  <object class="GtkBox" id="box">
-    <property name="visible">True</property>
-    <property name="can_focus">False</property>
-    <child>
-      <object class="GtkRadioButton" id="TabButton">
-        <property name="label" translatable="yes">Tab</property>
-        <property name="visible">True</property>
-        <property name="can_focus">True</property>
-        <property name="receives_default">False</property>
-        <property name="image">TabIcon</property>
-        <property name="xalign">0</property>
-        <property name="active">True</property>
-        <property name="draw_indicator">False</property>
-      </object>
-      <packing>
-        <property name="expand">False</property>
-        <property name="fill">False</property>
-        <property name="position">0</property>
-      </packing>
-    </child>
-    <child>
-      <object class="GtkButton" id="Close">
-        <property name="visible">True</property>
-        <property name="can_focus">True</property>
-        <property name="receives_default">True</property>
-        <property name="halign">start</property>
-        <property name="valign">start</property>
-        <property name="image">CloseIcon</property>
-        <property name="relief">none</property>
-      </object>
-      <packing>
-        <property name="expand">False</property>
-        <property name="fill">False</property>
-        <property name="position">2</property>
-      </packing>
-    </child>
-  </object>
-</interface>
-'''
+class TAB_UI(Gtk.StackSwitcher):
+    def __init__(self):
+        super(TAB_UI, self).__init__()
+
