3ce4a76705119cddf69ebbaac4d2146c7da861d6
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-31 12:46:57 +0200

    / ‘grabbo/__init__.py’

--- grabbo/__init__.py
+++ grabbo/__init__.py
@@ -1,7 +1,7 @@
 #from grabbo import ui
 from grabbo.builder import Builder
 from grabbo.window import Window
-from grabbo.notebook import Notebook
+from grabbo.notebook import Notebook, TabButton
 from grabbo.list import ListElement
 
 
