b63890df1167fced13e8d65ab5e6a79c9b035d90
Jeremi Biernacki <jeremi360@gmail.com>
2014-08-31 15:19:50 +0200

    / ‘grabbo/notebook.py’

--- grabbo/notebook.py
+++ grabbo/notebook.py
@@ -1,44 +1,40 @@
 from gi.repository import Gtk
 import os
+import grabbo
 
 r = os.path.realpath(__file__)
-LIST_UI = os.path.join(r, '..', 'ui', 'Tabs.xml')
+CB_UI = os.path.join(r, '..', 'ui', 'CloseButton.xml')
+
+class _CloseButton(grabbo.Builder):
+    def __init__(self, notebook, content):
+        super(_CloseButton, self).__init__(CB_UI)
+        self.n = notebook
+        self.c = content
 
     def on_it(self, button):
         self.n.stack.remove(self.c)
         self.n.switcher.remove(self)
 
+
+LIST_UI = os.path.join(r, '..', 'ui', 'Tabs.xml')
+
 class Notebook(Gtk.Box):
     def __init__(self, stack = Gtk.Stack(), addable = True, closeable = True, orientation = Gtk.Orientation.HORIZONTAL):
         super(Notebook, self).__init__()
+
         self.set_orientation(orientation)
-
-        img = Gtk.Image()
-        img.new_from_icon_name("list-add", 4)
-        self.AddButton = Gtk.Button("add")
-        self.AddButton.set_image(img)
-        self.AddButton.set_always_show_image(True)
-        self.AddButton.set_vexpand(False)
-        self.AddButton.set_hexpand(False)
-
         self.stack = stack
 
         self.set_addable(addable)
-        self.set_orientation(orientation)
         self.AddButton.connect("clicked", self.on_add)
 
         self.switcher = Gtk.StackSwitcher()
         self.switcher.set_stack(self.stack)
 
-        vp = Gtk.Viewport()
-        self.switcher.show()
         vp.add(self.switcher)
         self._sc = Gtk.ScrolledWindow()
-        vp.show()
-        self._sc.add(vp)
-        self._sc.show()
-        self.pack_start(self._sc, False, False, 0)
-        self.add(self.AddButton)
+
+
 
     def on_add(self, button):
         content = Gtk.Label()
@@ -55,6 +51,6 @@ class Notebook(Gtk.Box):
             cb.show()
 
     def set_addable(self, addable):
-        if not addable:
-            self.AddButton.hide()
+        if addable:
+            self.AddButton.show()
 
