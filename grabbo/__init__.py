f80135ba26e4c6008c2e707c15442149ec4c8742
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-17 08:57:53 +0200

    / ‘grabbo/__init__.py’

--- grabbo/__init__.py
+++ grabbo/__init__.py
@@ -2,7 +2,7 @@ from grabbo.builder import Builder
 from grabbo.window import Window
 from grabbo.notebook import Notebook, TabButton
 #from grabbo.list import ListItem, List - not done yet
-#from grabbo.edit import EditBox - not done yet
+from grabbo.edit import EditBox
 
 
 
