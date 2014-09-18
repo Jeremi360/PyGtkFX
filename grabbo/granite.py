bacfc4f75ac340415e87135c0979fea984d12a2d
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-18 15:15:46 +0200

    / ‘grabbo/granite.py’

--- grabbo/granite.py
+++ grabbo/granite.py
@@ -4,6 +4,7 @@ from gi.repository.Granite import WidgetsAboutDialog as AboutDialog
 from gi.repository.Granite import WidgetsAppMenu as AppMenu
 from gi.repository.Granite import WidgetsPopOver as PopOver
 from gi.repository.Granite import WidgetsCollapsiblePaned as CollapsiblePaned
+from gi.repository.Granite import WidgetsCompositedWindow as CompositedWindow
 from gi.repository.Granite import WidgetsDatePicker as DatePicker
 from gi.repository.Granite import WidgetsWelcome as Welcome
 
