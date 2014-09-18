e81c98d47515502d1b7b0dc87d651edd89b30968
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-18 15:20:18 +0200

    / ‘grabbo/granite.py’

--- grabbo/granite.py
+++ grabbo/granite.py
@@ -1,6 +1,5 @@
 # make imports stuff from granite easy
 
-
 from gi.repository.Granite import WidgetsAboutDialog as AboutDialog
 from gi.repository.Granite import WidgetsAppMenu as AppMenu
 from gi.repository.Granite import WidgetsPopOver as PopOver
