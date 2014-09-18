9c4757caaf3f545a3ddd79bb2f3dd9964b6d2cd5
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-18 15:17:57 +0200

    / ‘grabbo/granite.py’

--- grabbo/granite.py
+++ grabbo/granite.py
@@ -1,13 +1,14 @@
 # make imports stuff from granite easy
 
-from gi.repository.Granite import WidgetsAboutDialog as AboutDialog
-from gi.repository.Granite import WidgetsAppMenu as AppMenu
-from gi.repository.Granite import WidgetsPopOver as PopOver
-from gi.repository.Granite import WidgetsCollapsiblePaned as CollapsiblePaned
-from gi.repository.Granite import WidgetsCompositedWindow as CompositedWindow
-from gi.repository.Granite import WidgetsDatePicker as DatePicker
-from gi.repository.Granite import WidgetsWelcome as Welcome
+class Widgets:
+    from gi.repository.Granite import WidgetsAboutDialog as AboutDialog
+    from gi.repository.Granite import WidgetsAppMenu as AppMenu
+    from gi.repository.Granite import WidgetsPopOver as PopOver
+    from gi.repository.Granite import WidgetsCollapsiblePaned as CollapsiblePaned
+    from gi.repository.Granite import WidgetsCompositedWindow as CompositedWindow
+    from gi.repository.Granite import WidgetsDatePicker as DatePicker
+    from gi.repository.Granite import WidgetsWelcome as Welcome
 
-class CellRenderer:
-    from gi.repository.Granite import WidgetsCellRendererBadge as Badge
-    from gi.repository.Granite import WidgetsCellRendererExpander as Expander
+    class CellRenderer:
+        from gi.repository.Granite import WidgetsCellRendererBadge as Badge
+        from gi.repository.Granite import WidgetsCellRendererExpander as Expander
