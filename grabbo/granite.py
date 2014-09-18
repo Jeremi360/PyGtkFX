dc1f13bac5eb2a5b15315e540f432f91551e8da1
Jeremi Biernacki <jeremi360@gmail.com>
2014-09-18 15:19:14 +0200

    / ‘grabbo/granite.py’

--- grabbo/granite.py
+++ grabbo/granite.py
@@ -8,7 +8,6 @@ class Widgets:
     from gi.repository.Granite import WidgetsCompositedWindow as CompositedWindow
     from gi.repository.Granite import WidgetsDatePicker as DatePicker
     from gi.repository.Granite import WidgetsWelcome as Welcome
-    from gi.repository.Granite import WidgetsPopOver as PopOver
 
     class CellRenderer:
         from gi.repository.Granite import WidgetsCellRendererBadge as Badge
