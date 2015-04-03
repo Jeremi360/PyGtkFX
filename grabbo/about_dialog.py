82aee3a72bab72b4657d304a434eb72b0fdbb8d5
jeremi360 <jeremi360@gmail.com>
2015-04-03 10:07:57 +0200

    / ‘grabbo/about_dialog.py’

--- grabbo/about_dialog.py
+++ grabbo/about_dialog.py
@@ -61,20 +61,20 @@ class AboutDialog(grabbo.Window):
         self._Name = Gtk.Label()
         self._Name.set_markup("<b>AppName</b>")
         self._InfoBox.add(self._Name)
-        self._Name.set_vexpand(False)
+        self._Name.set_vexpand_set(False)
         
         self._ShortDescrpition = Gtk.Label("Awesome App")
         self._InfoBox.add(self._ShortDescrpition)
-        self._ShortDescrpition.set_vexpand(False)
+        self._ShortDescrpition.set_vexpand_set(False)
         
         self._Version = Gtk.Label("0.3")
         self._InfoBox.add(self._Version)
-        self._Version.set_vexpand(False)
+        self._Version.set_vexpand_set(False)
         
         self._TextView = Gtk.TextView()
         self._scrolledwindow1 = Gtk.ScrolledWindow()
         self._scrolledwindow1.add(self._TextView)
-        self._scrolledwindow1.set_vexpand(True)
+        self._scrolledwindow1.set_vexpand_set(True)
         self._InfoBox.add(self._scrolledwindow1)
         
         self.add(self._InfoBox)
