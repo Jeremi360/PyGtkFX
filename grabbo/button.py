f3e54982ddd277fae1c201e36d1349635fa3e77c
jeremi360 <jeremi360@gmail.com>
2015-04-09 00:21:34 +0200

    / ‘grabbo/button.py’

--- grabbo/button.py
+++ grabbo/button.py
@@ -2,7 +2,7 @@ from gi.repository import Gtk
 
 
 class StandardButton(Gtk.Button):
-    def __init__(self, label = "Button", img = Gtk.Image().new_from_icon_name("dialog-information")):
+    def __init__(self, label = "Button", img = Gtk.Image().new_from_icon_name("dialog-information", 4)):
         Gtk.Button.__init__(self)
         self.set_label(label)
         self.set_relief(Gtk.ReliefStyle.NONE)
